11729806f37f6f9df4805bed4ba9c646907c700d
Chebii55 <102931955+Chebii55@users.noreply.github.com>
2022-10-18 05:07:46 +0300

    Add files via upload

---
+++ MyProject/src/myproject/MyProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ICDLLAB3
+ */
+public class MyProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     JOptionPane.showMessageDialog(null,"Soea programming");     // TODO code application logic here
+    }
+}
---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author ICDLLAB3
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
