dfe319d399ea7f51cbd0c2b0b1fdddf4637c0b50
Chebii55 <102931955+Chebii55@users.noreply.github.com>
2022-11-23 09:26:08 +0300

    Add files via upload

---
+++ mYpROJECT3/src/myproject3/MYpROJECT3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject3;
+
+/**
+ *
+ * @author Job
+ */
+public class MYpROJECT3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
